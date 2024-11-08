641eebd6a564f4a94b633441031a7f0b73321a28
unknown <emmafinnegan29@gmail.com>
2024-11-08 18:03:32 -0300

    correcciones

--- LoginAndRegister/src/components/Tickets/TicketService.tsx
+++ LoginAndRegister/src/components/Tickets/TicketService.tsx
@@ -28,29 +28,3 @@ export const crearTicket = async (ticket: Ticket) => {
 
 
 
-
-
-
-// // services/TicketService.ts
-// import apiClient from '../../api/apiService';
-// import { Ticket } from '../Tickets/Ticket';
-
-// const AGREGAR_TICKET_URL = '/ticket';
-
-// export const crearTicket = async (ticket: Ticket) => {
-//     const response = await apiClient.post(
-//         AGREGAR_TICKET_URL,
-//         JSON.stringify({
-//             cantidadDeSolicitud: ticket.cantidadDeSolicitud,
-//             fechaDeSolicitud: ticket.fechaDeSolicitud,
-//             estado: ticket.estado,
-//             vehiculo: { patente: ticket.vehiculo.patente },  
-//             usuario: { id: ticket.usuario.id }                
-//         }),
-//         {
-//             headers: { 'Content-Type': 'application/json' },
-//             withCredentials: true,
-//         }
-//     );
-//     return response;
-// };
