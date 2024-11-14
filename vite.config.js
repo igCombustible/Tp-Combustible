5d080db9f27e6651ceae585b0b893b7b8b71992a
Alan Rodas Bonjour <alanrodas@gmail.com>
2024-11-14 19:46:24 -0300

    chore: Adding support for docker and docker compose

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import process from 'process'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
+})
